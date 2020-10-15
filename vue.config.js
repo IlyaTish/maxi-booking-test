a49d03e2ff5a84224332fad3b689161818678fa7
Илья Тищенко <ilya.tishencko676@yandex.ru>
2020-10-15 09:26:15 +0300

    Edit vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   chainWebpack: config => {
-      config.module.rules.delete('eslint');
-  }
+    config.module.rules.delete('eslint');
+  },
+  publicPath: '/maxi-booking-test/'
 }
